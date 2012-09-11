89622bae72b634669d7637c1a407ed6ac3bd42cf
Amndeep Mann <2013amann@axiom.csl.tjhsst.edu>
2012-09-11 15:51:33 -0400

    Implemented some functionality for opening images.

--- src/photographicmosaic/imageio/ImageManipulator.java
+++ src/photographicmosaic/imageio/ImageManipulator.java
@@ -1,32 +1,52 @@
 package photographicmosaic.imageio;
 
-import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public final class ImageManipulator
-{
+{	
 	public ImageManipulator()
 	{
 	}
 	
-	public static int[] averagePixelValues(Image image, boolean grayscale)
+	public static int[] averagePixelValues(BufferedImage image, boolean grayscale)
 	{
-		return null;
+		return averagePixelValuesPerArea(image, grayscale, 0, 0, image.getWidth(), image.getHeight());
 	}
 	
-	public static Image getImage(String filename)
+	public static BufferedImage getImage(String filename) throws IOException
 	{
-		return null;
+		return ImageIO.read(new File(filename));
 	}
 	
-	public static Image createImage(Image[][] subimages)
+	public static BufferedImage createImage(BufferedImage[][] subimages)
 	{
 		return null;
 	}
 	
 	//perhaps include a method that identifies the average pixel values for portions of the image? maybe like this?
-	public static int[][][] averagePixelValuesPerArea(Image image, boolean grayscale, int unitsWide, int unitsHigh)
+	public static int[] averagePixelValuesPerArea(BufferedImage image, boolean grayscale, int x, int y, int width, int height)
 	{
 		return null;
+	}
+
+	public static void main(String[] args)
+	{
+		BufferedImage image = null;
+		boolean doesNotHaveValidName = true;
+		while(doesNotHaveValidName)
+			try
+			{
+				image = ImageManipulator.getImage("test.jpg");
+				doesNotHaveValidName = false;
+			}
+			catch(IOException e)
+			{}
+				
+		System.out.println(image);
+		System.out.println(image.getWidth());
+		System.out.println(image.getHeight());
 	}
 }
