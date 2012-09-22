c0768e227a0041e6a31b624b7c0af7edd39597a4
Amndeep Singh Mann <Amndeep.dev@gmail.com>
2012-09-22 00:14:09 -0400

    Finished implementing creation of merged pictures.

--- src/photographicmosaic/imageio/ImageManipulator.java
+++ src/photographicmosaic/imageio/ImageManipulator.java
@@ -1,6 +1,5 @@
 /*
- *  Compiling: in path/src/photographicmosaic/imageio javac -d . ImageManipulator.java
- *  Running:   in path/src/                           java -cp photographicmosaic/imageio/ photographicmosaic.imageio.ImageManipulator
+ * Compiling: in path/src/photographicmosaic/imageio javac -d . ImageManipulator.java Running: in path/src/ java -cp photographicmosaic/imageio/ photographicmosaic.imageio.ImageManipulator
  */
 
 package photographicmosaic.imageio;
@@ -11,27 +10,60 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ImageManipulator
-{	
+{
 	public ImageManipulator()
 	{
 	}
-	
+
 	public static int[] averagePixelValues(BufferedImage image, boolean grayscale)
 	{
 		return averagePixelValuesPerArea(image, grayscale, 0, 0, image.getWidth(), image.getHeight());
 	}
-	
+
 	public static BufferedImage getImage(String filename) throws IOException
 	{
 		return ImageIO.read(new File(filename));
 	}
-	
-	public static BufferedImage createImage(BufferedImage[][] subimages)
+
+	public static BufferedImage createImage(BufferedImage[][] subimages, int width, int height)
 	{
-		return null;
+		int tempwidth = width - width % subimages[0].length;
+		int tempheight = height - height % subimages.length;
+		BufferedImage temp = new BufferedImage(tempwidth, tempheight, subimages[0][0].getType());
+
+		if(subimages.length == 0)
+		{
+			if(subimages[0].length == 0)
+			{
+				temp.createGraphics().drawImage(subimages[0][0], 0, 0, tempwidth, tempheight, null);
+			}
+			else
+			{
+				for(int x = 0; x < subimages[0].length; x++)
+				{
+					temp.createGraphics().drawImage(subimages[0][x], x * tempwidth / subimages[0].length, 0, tempwidth / subimages[0].length, tempheight / subimages.length, null);
+				}
+			}
+		}
+		else
+		{
+			for(int y = 0; y < subimages.length; y++)
+			{
+				for(int x = 0; x < subimages[y].length; x++)
+				{
+					temp.createGraphics().drawImage(subimages[y][x], x * tempwidth / subimages[y].length, y * tempheight / subimages.length, tempwidth / subimages[y].length,
+					          tempheight / subimages.length, null);
+				}
+			}
+		}
+
+		BufferedImage ret = new BufferedImage(width, height, temp.getType());
+		ret.createGraphics().drawImage(temp, 0, 0, ret.getWidth(), ret.getHeight(), null);
+
+		return ret;
 	}
-	
-	//perhaps include a method that identifies the average pixel values for portions of the image? maybe like this?
+
+	// perhaps include a method that identifies the average pixel values for portions of the image? maybe like this?
 	public static int[] averagePixelValuesPerArea(BufferedImage image, boolean grayscale, int x, int y, int width, int height)
 	{
 		return null;
@@ -57,9 +89,42 @@ public class ImageManipulator
 					System.exit(1);
 				}
 			}
-				
-		System.out.println(image);
-		System.out.println(image.getWidth());
-		System.out.println(image.getHeight());
+
+		System.out.println(image + " " + image.getWidth() + " " + image.getHeight());
+		
+		BufferedImage[][] mergearray1 = new BufferedImage[1][1];
+		mergearray1[0][0] = image;
+		BufferedImage[][] mergearray2 = new BufferedImage[1][5];
+		for(int x = 0; x < 5; x++)
+		{
+			mergearray2[0][x] = image;
+		}
+		BufferedImage[][] mergearray3 = new BufferedImage[5][5];
+		for(int y = 0; y < 5; y++)
+		{
+			for(int x = 0; x < 5; x++)
+			{
+				mergearray3[y][x] = image;
+			}
+		}
+		
+		BufferedImage test1 = createImage(mergearray1, 500, 500);
+		BufferedImage test2 = createImage(mergearray2, 500, 500);
+		BufferedImage test3 = createImage(mergearray3, 500, 500);
+
+		System.out.println(test1 + " " + test1.getWidth() + " " + test1.getHeight());
+		System.out.println(test2 + " " + test2.getWidth() + " " + test2.getHeight());
+		System.out.println(test3 + " " + test3.getWidth() + " " + test3.getHeight());
+		
+		try{
+		ImageIO.write(test1, "jpg", new File("test1.jpg"));
+		ImageIO.write(test2, "jpg", new File("test2.jpg"));
+		ImageIO.write(test3, "jpg", new File("test3.jpg"));
+		}
+		catch(IOException e)
+		{
+			System.out.println("The image didn't become written.");
+			System.exit(1);
+		}
 	}
 }
